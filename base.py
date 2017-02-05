5ab3f7f7498f1d8bbf13c1d716b29da8e1b1b158
silentutopia <thenirajzade@gmail.com>
2017-02-05 20:29:03 +0530

    added base

---
+++ base.py
@@ -0,0 +1,14 @@
+#usr/bin/env python
+#use reset when struck
+from unicurses import *
+
+def main():
+    stdscr = initscr()
+
+
+
+    endwin()
+    return 0;
+
+if ( __name__ == "__main__" ):
+    main()
